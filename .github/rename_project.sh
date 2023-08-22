33739889246b0d4847355c79c16fc5faced934c3
s27y <s27y@users.noreply.github.com>
2023-08-22 15:25:39 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="s27y"
+original_name="test_project"
+original_urlname="test_project"
+original_description="Awesome test_project created by s27y"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv test_project $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ test_project/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for test_project."""
+
+from test_project.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
